b3382b963ad6fc969445bd8e66e7d40682bcac66
david.karlsboeck <david.karlsboeck@gmail.com>
2024-10-24 09:55:05 +0200

    collective improvements and fixes

--- fmt_eigen.h
+++ fmt_eigen.h
@@ -22,84 +22,110 @@
     SOFTWARE.
 */
 
-#ifndef FMT_EIGEN_HPP
+#ifndef FMT_EIGEN_H_
+#define FMT_EIGEN_H_
 
 #include <fmt/format.h>
+
 #include <Eigen/Dense>
-#include <regex>
-#include <iostream>
 #include <cstring>
 
-template<typename MatrixType>
+// Utility function to parse integers at compile-time
+constexpr int parse_integer(const char *str, std::size_t len) {
+  int value = 0;
+  for (std::size_t i = 0; i < len; ++i) {
+    if (str[i] >= '0' && str[i] <= '9') {
+      value = value * 10 + (str[i] - '0');
+    }
+  }
+  return value;
+}
+
+template <typename MatrixType>
 class MatrixFormatter : fmt::formatter<std::string> {
-private:
-    int precision = 6; // Default precision if not specified
-public:
-    // Parse the format string to extract the precision
-    template<typename ParseContext>
-    auto parse(ParseContext &ctx) {
-        auto it = ctx.begin();
-        auto end = ctx.end();
-        // Check if precision is specified in the format string
-        std::string input_str = std::string(it, end);
-        std::regex re(R"(\.(\d+))");
-        std::smatch match;
-        if (std::regex_search(input_str, match, re)) {
-            precision = std::stoi(match[1]);
-        }
-        while (it != end && *it != '}') it++;  // Skip to the end of the range
-        // Return the iterator after parsing
-        return it;
+ private:
+  int precision = 6;  // Default precision if not specified
+
+ public:
+  // Parse the format string to extract the precision
+  template <typename ParseContext>
+  constexpr auto parse(ParseContext &ctx) {
+    auto it = ctx.begin();
+    auto end = ctx.end();
+
+    // Look for '.' to specify precision
+    if (it != end && *it == '.') {
+      ++it;  // Skip '.'
+      auto precision_start = it;
+      while (it != end && *it >= '0' && *it <= '9') {
+        ++it;
+      }
+      // Convert to integer
+      precision = parse_integer(&(*precision_start),
+                                static_cast<size_t>(it - precision_start));
     }
 
-    template<typename Scalar, typename FormatContext>
-       typename std::enable_if<std::is_floating_point<Scalar>::value>::type
-       format_element(Scalar value, FormatContext &ctx) {
-        fmt::format_to(ctx.out(), "{:.{}f} ", value, precision);
+    // Skip to the end of the range
+    while (it != end && *it != '}') {
+      ++it;
     }
 
-    template<typename Scalar, typename FormatContext>
-    typename std::enable_if<std::is_integral<Scalar>::value>::type
-    format_element(Scalar value, FormatContext &ctx) {
-        fmt::format_to(ctx.out(), "{:d} ", value);
+    // Return the iterator after parsing
+    return it;
+  }
+
+  template <typename Scalar, typename FormatContext>
+  constexpr typename std::enable_if<std::is_floating_point<Scalar>::value>::type
+  format_element(Scalar value, FormatContext &ctx) const {
+    fmt::format_to(ctx.out(), "{:.{}f} ", value, precision);
+  }
+
+  template <typename Scalar, typename FormatContext>
+  constexpr typename std::enable_if<std::is_integral<Scalar>::value>::type
+  format_element(Scalar value, FormatContext &ctx) const {
+    fmt::format_to(ctx.out(), "{:d} ", value);
+  }
+
+  // Format the matrix with the specified precision
+  template <typename FormatContext>
+  auto format(const MatrixType &matrix, FormatContext &ctx) const {
+    for (int i = 0; i < matrix.rows(); ++i) {
+      for (int j = 0; j < matrix.cols(); ++j) {
+        format_element(matrix(i, j), ctx);
+      }
+      if (i != matrix.rows() - 1) {
+        fmt::format_to(ctx.out(), "\n");
+      }
     }
-
-    // Format the matrix with the specified precision
-    template<typename FormatContext>
-    auto format(const MatrixType &matrix, FormatContext &ctx) {
-        for (int i = 0; i < matrix.rows(); i++) {
-            for (int j = 0; j < matrix.cols(); j++) {
-                format_element(matrix(i, j), ctx);
-            }
-            if (i != matrix.rows() - 1) {
-                fmt::format_to(ctx.out(), "\n");
-            }
-        }
-        return ctx.out();
-    }
+    return ctx.out();
+  }
 };
 
-/// Specialize the formatter for Eigen::Matrix
-template<typename Scalar, int Rows, int Cols, int Options, int MaxRows, int MaxCols>
-struct fmt::formatter<Eigen::Matrix<Scalar, Rows, Cols, Options, MaxRows, MaxCols>> :
-        MatrixFormatter<Eigen::Matrix<Scalar, Rows, Cols, Options, MaxRows, MaxCols>> {
-};
+// Specialize the formatter for Eigen::Matrix
+template <typename Scalar, int Rows, int Cols, int Options, int MaxRows,
+          int MaxCols>
+struct fmt::formatter<
+    Eigen::Matrix<Scalar, Rows, Cols, Options, MaxRows, MaxCols>>
+    : MatrixFormatter<
+          Eigen::Matrix<Scalar, Rows, Cols, Options, MaxRows, MaxCols>> {};
 
-/// Specialize the formatter for Eigen::Transpose
-template<typename MatrixType>
-struct fmt::formatter<Eigen::Transpose<MatrixType>> : MatrixFormatter<Eigen::Transpose<MatrixType>> {
-};
+// Specialize the formatter for Eigen::Transpose
+template <typename MatrixType>
+struct fmt::formatter<Eigen::Transpose<MatrixType>>
+    : MatrixFormatter<Eigen::Transpose<MatrixType>> {};
 
-/// Specialize the formatter for Eigen::Block
-template<typename Scalar, int Rows, int Cols, int BlockRows, int BlockCols, bool InnerPanel>
-struct fmt::formatter<Eigen::Block<Eigen::Matrix<Scalar, Rows, Cols>, BlockRows, BlockCols, InnerPanel>> :
-        MatrixFormatter<Eigen::Block<Eigen::Matrix<Scalar, Rows, Cols>, BlockRows, BlockCols, InnerPanel>> {
-};
+// Specialize the formatter for Eigen::Block
+template <typename Scalar, int Rows, int Cols, int BlockRows, int BlockCols,
+          bool InnerPanel>
+struct fmt::formatter<Eigen::Block<Eigen::Matrix<Scalar, Rows, Cols>, BlockRows,
+                                   BlockCols, InnerPanel>>
+    : MatrixFormatter<Eigen::Block<Eigen::Matrix<Scalar, Rows, Cols>, BlockRows,
+                                   BlockCols, InnerPanel>> {};
 
-/// Specialize the formatter for Eigen::Diagno
-template<typename Scalar, int Rows, int Cols>
-struct fmt::formatter<Eigen::Diagonal<Eigen::Matrix<Scalar, Rows, Cols>>> :
-        MatrixFormatter<Eigen::Diagonal<Eigen::Matrix<Scalar, Rows, Cols>>> {
-};
+// Specialize the formatter for Eigen::Diagonal
+template <typename Scalar, int Rows, int Cols>
+struct fmt::formatter<Eigen::Diagonal<Eigen::Matrix<Scalar, Rows, Cols>>>
+    : MatrixFormatter<Eigen::Diagonal<Eigen::Matrix<Scalar, Rows, Cols>>> {};
 
-#endif
+
+#endif // FMT_EIGEN_H_
